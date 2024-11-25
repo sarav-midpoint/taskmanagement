484cd3e943633777913f3ea3a7df7d2dd0bd2a47
sarav-midpoint <saravanask0112@gmail.com>
2024-11-25 11:54:43 +0530

    add readme file

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Task } from "../redux/taskTypes";
 import { useDispatch } from "react-redux";
 import { addTask } from "../redux/actions";
+import "./styles.css";
 
 const TaskForm: React.FunctionComponent = () => {
   const [title, setTitle] = useState("");
@@ -23,15 +24,18 @@ const TaskForm: React.FunctionComponent = () => {
   };
 
   return (
-    <form action="" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Add New Task"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <button>Add Task</button>
-    </form>
+    <div className="container" id="formsub">
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add New Task"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <button>Add Task</button>
+      </form>
+    </div>
   );
 };
 
